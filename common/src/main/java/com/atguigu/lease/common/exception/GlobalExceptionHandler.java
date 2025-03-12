4c274d97c7b170830938d3bc48811b36a9cc537e
夏叶城二 <xl18374176709@qq.com>
2025-03-12 21:42:13 +0800

    common

---
+++ common/src/main/java/com/atguigu/lease/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.atguigu.lease.common.exception;
+
+import com.atguigu.lease.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.rmi.dgc.Lease;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result handle(Exception e) {
+        e.printStackTrace();
+        return Result.fail();
+    }
+
+    @ExceptionHandler(LeaseException.class)
+    @ResponseBody
+    public Result handle(LeaseException e) {
+        e.printStackTrace();
+        return Result.fail(e.getCode(), e.getMessage());
+    }
+}
